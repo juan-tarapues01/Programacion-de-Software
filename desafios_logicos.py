790be34a0612477c73a63d775ee21b37651bf505
juan tarapues <jftarapuesch24@gmail.com>
2025-08-08 15:48:57 -0500

    digramas #2

--- desafios_logicos.py
+++ desafios_logicos.py
@@ -3,7 +3,7 @@ print("--------ejercicio_1------------")
 
 edad = int(input("Ingresa tu edad: "))
 if edad < 18:
-    print("Eres menor de edad.")
+    print("Eres menor de edad.")#####
 elif edad >= 65:
     print("Eres adulto mayor.")
 else:
