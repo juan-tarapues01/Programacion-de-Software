f80cfa1d60a7cdc9862cbb8210d03ad35bb3a4a7
juan tarapues <jftarapuesch24@gmail.com>
2025-08-22 17:40:49 -0500

    Cambios dentro del submódulo

---
+++ todo de programacion/bucle.py
@@ -0,0 +1,60 @@
+# num= int(input("ingrese un numero: "))
+
+# while num>0:
+#     print(f"{num}")
+#     num -= 1
+# print("Terminó el conteo")
+
+
+
+
+# numero = int(input( "ingrese numero de la tabla  de multiplicar: "))
+
+
+# i=1 #inicialización
+# print(f"\ninicia el contador en 1 {numero}")
+
+# while i <=10:
+#     print(f"{numero}*{i}= {numero*i}")
+#     i += 1
+# print("Termino")    
+
+
+
+# numero2=int(input("ingrese un numero: "))
+
+# while True:
+#     numero2 -=1
+#     print(numero2)
+#     if numero2 == 0:
+#         break
+# print("fin del bucle")
+
+
+# x = 5
+# while True:
+#     x -=1
+#     print(x)
+#     if x == 0:
+#         break
+# print("Fn del bucle")
+
+"ejercicio_1"
+
+suma = 0
+numero = int(input("ingrese un numero: ")),int(input("ingrese un numero: ")),int(input("ingrese un numero: "))
+
+while numero ==0: 
+    print(numero*suma)
+    if numero==1:
+        print("tu numero es cero")
+        break
+    print ("fin")# esto es es un print
+
+
+
+        
+
+    
+
+
